1ab98cef9724b658cba3c48d44ba25fd2bb2bc5b
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-18 12:44:26 -0800

    completed MVP

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,9 @@
 const db = require('../../data/dbConfig');
 
 function getTasks() {
-  return db('tasks');
+  return db('tasks as t')
+    .join('projects as p', 't.project_id', '=', 'p.project_id')
+    .select('t.*', 'p.project_name', 'p.project_description')
 };
 
 function createTask(task) {
@@ -10,7 +12,6 @@ function createTask(task) {
       return db('tasks').where('task_id', task_id).first();
     });
 };
-
 
 module.exports = {
   getTasks,
